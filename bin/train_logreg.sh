455a34b2c8c053779b3286493ef1347fdc6bd6f6
fursovia <fursov.ia@gmail.com>
2020-05-01 17:43:08 +0300

    FIX: removed outdated bash scripts

--- bin/train_logreg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# bash bin/train_logreg.sh {DATA_DIR}
-
-DATA_DIR=$1
-MODEL_DIR=experiments/$(basename ${DATA_DIR})/logreg
-
-python train_logreg.py --data_dir ${DATA_DIR} --model_dir ${MODEL_DIR}
