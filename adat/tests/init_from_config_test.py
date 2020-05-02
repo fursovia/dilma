c9cb16c50d32916ae222fc266fc301ef0955899f
fursovia <fursov.ia@gmail.com>
2020-05-03 02:24:15 +0300

    FIX: removed env vars

--- adat/tests/init_from_config_test.py
+++ adat/tests/init_from_config_test.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from glob import glob
-import os
 
 from allennlp.data import Vocabulary
 from allennlp.common import Params
@@ -10,15 +9,6 @@ from adat.models import DeepLevenshtein, MaskedLanguageModel, BasicClassifierOne
 
 PROJECT_ROOT = (Path(__file__).parent / ".." / "..").resolve()
 
-os.environ["LM_TRAIN_DATA_PATH"] = ""
-os.environ["LM_VALID_DATA_PATH"] = ""
-os.environ["CLS_TRAIN_DATA_PATH"] = ""
-os.environ["CLS_VALID_DATA_PATH"] = ""
-os.environ["DL_TRAIN_DATA_PATH"] = ""
-os.environ["DL_VALID_DATA_PATH"] = ""
-os.environ["LM_VOCAB_PATH"] = ""
-os.environ["CLS_NUM_CLASSES"] = "2"
-
 
 def test_deep_levenshtein_configs():
 
@@ -26,7 +16,14 @@ def test_deep_levenshtein_configs():
     assert len(paths) > 0
     for config_path in paths:
         try:
-            params = Params.from_file(str(config_path))
+            params = Params.from_file(
+                str(config_path),
+                ext_vars={
+                    "DL_TRAIN_DATA_PATH": "",
+                    "DL_VALID_DATA_PATH": "",
+                    "LM_VOCAB_PATH": ""
+                }
+            )
             blank_vocab = Vocabulary()
             params["model"].pop("type")
             DeepLevenshtein.from_params(params=params["model"], vocab=blank_vocab)
@@ -40,7 +37,13 @@ def test_masked_lm_configs():
     assert len(paths) > 0
     for config_path in paths:
         try:
-            params = Params.from_file(str(config_path))
+            params = Params.from_file(
+                str(config_path),
+                ext_vars={
+                    "LM_TRAIN_DATA_PATH": "",
+                    "LM_VALID_DATA_PATH": ""
+                }
+            )
             blank_vocab = Vocabulary(tokens_to_add={"tokens": ["@@MASK@@"]})
             params["model"].pop("type")
             MaskedLanguageModel.from_params(params=params["model"], vocab=blank_vocab)
@@ -54,7 +57,15 @@ def test_classifier_configs():
     assert len(paths) > 0
     for config_path in paths:
         try:
-            params = Params.from_file(str(config_path))
+            params = Params.from_file(
+                str(config_path),
+                ext_vars={
+                    "CLS_TRAIN_DATA_PATH": "",
+                    "CLS_VALID_DATA_PATH": "",
+                    "CLS_NUM_CLASSES": "2",
+                    "LM_VOCAB_PATH": ""
+                }
+            )
             blank_vocab = Vocabulary()
             params["model"].pop("type")
             BasicClassifierOneHotSupport.from_params(params=params["model"], vocab=blank_vocab)
