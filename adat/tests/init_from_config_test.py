c649d1bcc76ed55226cb6d1dab57443643584c16
fursovia <fursov.ia@gmail.com>
2020-05-03 02:10:22 +0300

    ADD: gru classifier

---
+++ adat/tests/init_from_config_test.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+from glob import glob
+import os
+
+from allennlp.data import Vocabulary
+from allennlp.common import Params
+
+from adat.models import DeepLevenshtein, MaskedLanguageModel, BasicClassifierOneHotSupport
+
+
+PROJECT_ROOT = (Path(__file__).parent / ".." / "..").resolve()
+
+os.environ["LM_TRAIN_DATA_PATH"] = ""
+os.environ["LM_VALID_DATA_PATH"] = ""
+os.environ["CLS_TRAIN_DATA_PATH"] = ""
+os.environ["CLS_VALID_DATA_PATH"] = ""
+os.environ["DL_TRAIN_DATA_PATH"] = ""
+os.environ["DL_VALID_DATA_PATH"] = ""
+os.environ["LM_VOCAB_PATH"] = ""
+os.environ["CLS_NUM_CLASSES"] = "2"
+
+
+def test_deep_levenshtein_configs():
+
+    paths = glob(str(PROJECT_ROOT / "training_config/levenshtein/*.jsonnet"))
+    assert len(paths) > 0
+    for config_path in paths:
+        try:
+            params = Params.from_file(str(config_path))
+            blank_vocab = Vocabulary()
+            params["model"].pop("type")
+            DeepLevenshtein.from_params(params=params["model"], vocab=blank_vocab)
+        except Exception as e:
+            raise AssertionError(f"unable to load params from {config_path}, because {e}")
+
+
+def test_masked_lm_configs():
+
+    paths = glob(str(PROJECT_ROOT / "training_config/lm/*.jsonnet"))
+    assert len(paths) > 0
+    for config_path in paths:
+        try:
+            params = Params.from_file(str(config_path))
+            blank_vocab = Vocabulary(tokens_to_add={"tokens": ["@@MASK@@"]})
+            params["model"].pop("type")
+            MaskedLanguageModel.from_params(params=params["model"], vocab=blank_vocab)
+        except Exception as e:
+            raise AssertionError(f"unable to load params from {config_path}, because {e}")
+
+
+def test_classifier_configs():
+
+    paths = glob(str(PROJECT_ROOT / "training_config/classifier/*.jsonnet"))
+    assert len(paths) > 0
+    for config_path in paths:
+        try:
+            params = Params.from_file(str(config_path))
+            blank_vocab = Vocabulary()
+            params["model"].pop("type")
+            BasicClassifierOneHotSupport.from_params(params=params["model"], vocab=blank_vocab)
+        except Exception as e:
+            raise AssertionError(f"unable to load params from {config_path}, because {e}")
---
+++ training_config/classifier/gru_classifier.jsonnet
@@ -0,0 +1,74 @@
+local TOKEN_INDEXER = {
+    "tokens": {
+        "type": "single_id",
+        "start_tokens": [
+          "<START>"
+        ],
+        "end_tokens": [
+          "<END>"
+        ],
+        // should be set to the maximum value of `ngram_filter_sizes`
+        "token_min_padding_length": 5
+      }
+};
+
+{
+  "dataset_reader": {
+    "type": "text_classification_json",
+    // DO NOT CHANGE token_indexers
+    "token_indexers": TOKEN_INDEXER,
+    // DO NOT CHANGE tokenizer
+    "tokenizer": {
+      "type": "just_spaces"
+    },
+    "skip_label_indexing": true,
+    "lazy": false
+  },
+  "train_data_path": std.extVar("CLS_TRAIN_DATA_PATH"),
+  "validation_data_path": std.extVar("CLS_VALID_DATA_PATH"),
+  // Make sure you load vocab from LM
+  "vocabulary": {
+    "type": "from_files",
+    "directory": std.extVar("LM_VOCAB_PATH")
+  },
+  "model": {
+    "type": "basic_classifier_one_hot_support",
+    "text_field_embedder": {
+      "token_embedders": {
+        "tokens": {
+          "type": "embedding",
+          "embedding_dim": 100,
+          "trainable": true
+        }
+      }
+    },
+    "seq2seq_encoder": {
+        "type": "gru",
+        "input_size": 100,
+        "hidden_size": 128,
+        "num_layers": 1,
+        "dropout": 0.1,
+        "bidirectional": true
+    },
+    "seq2vec_encoder": {
+      "type": "bag_of_embeddings",
+      "embedding_dim": 256,
+      "averaged": true
+    },
+    "dropout": 0.2,
+    "num_labels": std.parseInt(std.extVar("CLS_NUM_CLASSES"))
+  },
+  "data_loader": {
+    "batch_size": 64
+  },
+  "distributed": {
+    "cuda_devices": [
+      2,
+      3
+    ]
+  },
+  "trainer": {
+    "num_epochs": 50,
+    "patience": 3
+  }
+}
