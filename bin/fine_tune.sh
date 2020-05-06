91c961d0508180a4016cf55a3d32f15fb825058d
fursovia <fursov.ia@gmail.com>
2020-05-06 18:52:02 +0300

    ADD: gpu id

--- bin/fine_tune.sh
+++ bin/fine_tune.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # usage
-# bash bin/train_models.sh {LOG_DIR} {TRAIN_PATH}
+# bash bin/train_models.sh {LOG_DIR} {TRAIN_PATH} {GPU_ID}
 
 LOG_DIR=$1
 TRAIN_PATH=$2
 VALID_PATH=$3
+GPU_ID=${3:-"-1"}
 
 
 allennlp train ${LOG_DIR}/config.json \
     -s ${LOG_DIR} \
     --include-package adat \
-    --overrides {"trainer.num_epochs": 1, "trainer.patience": 1, "train_data_path": ${TRAIN_PATH}, "validation_data_path": ${VALID_PATH}, "distributed": null} \
+    --overrides '{"trainer.num_epochs": 1, "trainer.patience": 1, "train_data_path": ${TRAIN_PATH}, "validation_data_path": ${VALID_PATH}, "distributed": null, "trainer.cuda_device": ${GPU_ID}}' \
     --recover
