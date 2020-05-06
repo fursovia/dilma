407a484f2b6dd6a7c80a130ae021991c1f994b32
fursovia <fursov.ia@gmail.com>
2020-05-06 17:01:09 +0300

    FIX: lev dataset creation speed up

--- adat/utils.py
+++ adat/utils.py
@@ -4,6 +4,7 @@ from multiprocessing import Pool
 from typing import Sequence, Dict, Any, List
 import json
 import re
+import random
 from IPython.core.display import display, HTML
 
 import torch
@@ -101,8 +102,13 @@ def clean_sequence(sequence: str) -> str:
 
 class SequenceModifier:
 
-    def __init__(self, vocab: List[str], remove_prob: float = 0.05,
-                 add_prob: float = 0.05, replace_prob: float = 0.05) -> None:
+    def __init__(
+            self,
+            vocab: List[str],
+            remove_prob: float = 0.05,
+            add_prob: float = 0.05,
+            replace_prob: float = 0.1
+    ) -> None:
         self.vocab = vocab
         self.remove_prob = remove_prob
         self.add_prob = add_prob
@@ -113,34 +119,22 @@ class SequenceModifier:
         sequence = [token for i, token in enumerate(sequence) if not samples[i]]
         return sequence
 
-    def add_token(self, sequence: List[str]) -> List[str]:
-        samples = np.random.binomial(n=1, p=self.replace_prob, size=len(sequence))
-
-        new_sequence = []
-        for i, token in enumerate(sequence):
-            new_sequence.append(token)
-            if samples[i]:
-                random_token = np.random.choice(self.vocab)
-                new_sequence.append(random_token)
-        return new_sequence
-
     def replace_token(self, sequence: List[str]) -> List[str]:
         samples = np.random.binomial(n=1, p=self.replace_prob, size=len(sequence))
+        new_sequence = [random.choice(self.vocab) if samples[i] else sequence[i] for i in range(len(sequence))]
+        return new_sequence
 
-        new_sequence = []
-        for i, token in enumerate(sequence):
-            if samples[i]:
-                random_token = np.random.choice(self.vocab)
-                new_sequence.append(random_token)
-            else:
-                new_sequence.append(token)
+    def add_token(self, sequence: List[str]) -> List[str]:
+        new_sequence = sequence + [
+            random.choice(self.vocab)
+            for _ in range(np.random.binomial(len(sequence), self.add_prob))
+        ]
         return new_sequence
 
     def __call__(self, sequence: str) -> str:
         splitted_sequence = sequence.split()
         if len(splitted_sequence) > 1:
             splitted_sequence = self.remove_token(splitted_sequence)
-
         splitted_sequence = self.replace_token(splitted_sequence)
         splitted_sequence = self.add_token(splitted_sequence)
         return " ".join(splitted_sequence)
--- scripts/create_levenshtein_dataset.py
+++ scripts/create_levenshtein_dataset.py
@@ -14,7 +14,14 @@ parser.add_argument("--output-dir", type=str, required=True)
 parser.add_argument("--field-name", type=str, default="text")
 parser.add_argument("--num-adversarial", type=int, default=200000)
 parser.add_argument("--num-non-adversarial", type=int, default=30000)
-parser.add_argument("--test-size", type=float, default=0.15)
+parser.add_argument("--add-prob", type=float, default=0.1)
+parser.add_argument("--replace-prob", type=float, default=None)
+parser.add_argument("--remove-prob", type=float, default=None)
+parser.add_argument("--test-size", type=float, default=None)
+
+NUM_REMOVES_IN_AVERAGE = 1.0
+NUM_REPLACE_IN_AVERAGE = 2.0
+NUM_ADD_IN_AVERAGE = 1.0
 
 
 if __name__ == "__main__":
@@ -27,11 +34,18 @@ if __name__ == "__main__":
 
     data = load_jsonlines(args.data_path)
     sequences = [str(el[args.field_name]) for el in data]
+    mean_len = float(np.mean([len(seq.split()) for seq in sequences]))
     vocab = []
     for seq in sequences:
         vocab.extend(seq.split())
     vocab = list(set(vocab))
-    modifier = SequenceModifier(vocab)
+
+    modifier = SequenceModifier(
+        vocab,
+        remove_prob=args.remove_prob or NUM_REMOVES_IN_AVERAGE / mean_len,
+        add_prob=args.add_prob or NUM_ADD_IN_AVERAGE / mean_len,
+        replace_prob=args.replace_prob or NUM_REPLACE_IN_AVERAGE / mean_len
+    )
 
     dataset = []
     non_adversarial_indexes = np.random.randint(0, len(sequences), size=(args.num_non_adversarial, 2))
