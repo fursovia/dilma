f7d0c6b6f719921fa2c3a22585ad80feab83744f
fursovia <fursov.ia@gmail.com>
2020-05-06 18:43:30 +0300

    ADD: fine-tuning scripts

---
+++ bin/fine_tune.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# usage
+# bash bin/train_models.sh {LOG_DIR} {TRAIN_PATH}
+
+LOG_DIR=$1
+TRAIN_PATH=$2
+VALID_PATH=$3
+
+
+allennlp train ${LOG_DIR}/config.json \
+    -s ${LOG_DIR} \
+    --include-package adat \
+    --overrides {"trainer.num_epochs": 1, "trainer.patience": 1, "train_data_path": ${TRAIN_PATH}, "validation_data_path": ${VALID_PATH}} \
+    --recover
---
+++ scripts/prepare_for_fine_tuning.py
@@ -0,0 +1,27 @@
+import argparse
+from pathlib import Path
+import jsonlines
+
+from adat.utils import load_jsonlines
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--adversarial-dir", type=str, required=True)
+parser.add_argument("--max-wer", type=int, default=3)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    adversarial_dir = Path(args.adversarial_dir)
+
+    data = load_jsonlines(adversarial_dir / "attacked_data.json")
+
+    num_added = 0
+    data_path = adversarial_dir / "fine_tuning_data.json"
+    print(f"Saving data to {data_path}")
+    with jsonlines.open(data_path, "w") as writer:
+        for ex in data:
+            if ex["wer"] <= args.max_wer and ex["attacked_label"] != ex["adversarial_label"]:
+                num_added += 1
+                writer.write({"text": ex["sequence"], "label": ex["attacked_label"]})
+
+    print(f"Saved {num_added} examples.")
