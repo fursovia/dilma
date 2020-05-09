2c98c89c2c6930c4de4d4ac231203aba0f097ec6
fursovia <fursov.ia@gmail.com>
2020-05-09 16:19:30 +0300

    ADD: mix with train

--- scripts/prepare_for_fine_tuning.py
+++ scripts/prepare_for_fine_tuning.py
@@ -6,6 +6,7 @@ from adat.utils import load_jsonlines
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--adversarial-dir", type=str, required=True)
+parser.add_argument("--mix-with-path", type=str, default=None)
 parser.add_argument("--max-wer", type=int, default=3)
 
 
@@ -23,5 +24,10 @@ if __name__ == "__main__":
             if ex["wer"] <= args.max_wer and ex["attacked_label"] != ex["adversarial_label"]:
                 num_added += 1
                 writer.write({"text": ex["adversarial_sequence"], "label": ex["attacked_label"]})
+
+        if args.mix_with_path is not None:
+            data_to_mix_with = load_jsonlines(args.min_with_path)
+            for ex in data_to_mix_with:
+                writer.write(ex)
 
     print(f"Saved {num_added} examples.")
