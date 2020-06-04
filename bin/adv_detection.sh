b82921a016bc783f09540ce65e62e651d1c88a4a
fursovia <fursov.ia@gmail.com>
2020-06-05 00:21:11 +0300

    FIX: removed if

--- bin/adv_detection.sh
+++ bin/adv_detection.sh
@@ -16,33 +16,31 @@ DATASETS_DIR="datasets"
 for data_type in non_nlp nlp; do
     for result_dir in $(ls -d ${ATTACKS_DIR}/${data_type}/*); do
         dataset=$(basename ${result_dir})
-        if [[ "${dataset}" == "age" ]]; then
-            for dir in $(ls -d ${result_dir}/*); do
-                alg_name=$(basename ${dir})
-                echo ">>>> Preparing data for ${dataset} dataset, ${alg_name} algorithm"
-                PYTHONPATH=. python scripts/prepare_for_discr.py \
-                    --adversarial-dir ${dir} \
-                    --out-dir ${dir}/adv_detection
+        for dir in $(ls -d ${result_dir}/*); do
+            alg_name=$(basename ${dir})
+            echo ">>>> Preparing data for ${dataset} dataset, ${alg_name} algorithm"
+            PYTHONPATH=. python scripts/prepare_for_discr.py \
+                --adversarial-dir ${dir} \
+                --out-dir ${dir}/adv_detection
 
 
-                echo ">>>> Training ${dataset} dataset, ${alg_name} algorithm"
-                export DISCR_TRAIN_DATA_PATH=${dir}/adv_detection/train.json
-                export DISCR_VALID_DATA_PATH=${dir}/adv_detection/test.json
+            echo ">>>> Training ${dataset} dataset, ${alg_name} algorithm"
+            export DISCR_TRAIN_DATA_PATH=${dir}/adv_detection/train.json
+            export DISCR_VALID_DATA_PATH=${dir}/adv_detection/test.json
 
-                clf_dif=${LOGS_DIR}/${data_type}/dataset_${dataset}/target_clf/discriminator_${alg_name}
-                allennlp train configs/models/classifier/gru_discriminator.jsonnet \
-                    -s ${clf_dif} \
-                    --force \
-                    --include-package adat
+            clf_dif=${LOGS_DIR}/${data_type}/dataset_${dataset}/target_clf/discriminator_${alg_name}
+            allennlp train configs/models/classifier/gru_discriminator.jsonnet \
+                -s ${clf_dif} \
+                --force \
+                --include-package adat
 
-                echo ">>>> Evaluating Detector ${dataset} dataset, ${alg_name} algorithm"
-                PYTHONPATH=. python scripts/calculate_discr_metrics.py \
-                    --adversarial-dir ${dir} \
-                    --classifier-dir ${clf_dif} \
-                    --test-path ${DISCR_VALID_DATA_PATH} \
-                    --cuda ${GPU_ID}
-            done
-        fi
+            echo ">>>> Evaluating Detector ${dataset} dataset, ${alg_name} algorithm"
+            PYTHONPATH=. python scripts/calculate_discr_metrics.py \
+                --adversarial-dir ${dir} \
+                --classifier-dir ${clf_dif} \
+                --test-path ${DISCR_VALID_DATA_PATH} \
+                --cuda ${GPU_ID}
+        done
     done
 done
 
