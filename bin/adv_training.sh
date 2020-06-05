d7b7222fbd8e822e4552f1c9abdbaebaa7b5a83c
fursovia <fursov.ia@gmail.com>
2020-06-05 20:17:33 +0300

    FIX: final changes

--- bin/adv_training.sh
+++ bin/adv_training.sh
@@ -20,44 +20,40 @@ num=5000
 for data_type in non_nlp nlp; do
     for result_dir in $(ls -d ${ATTACKS_DIR}/${data_type}/*); do
         dataset=$(basename ${result_dir})
-        if [[ "${dataset}" != "age" ]]; then
-            for dir in $(ls -d ${result_dir}/*); do
-                alg_name=$(basename ${dir})
-                if [[ "${alg_name}" == "cascada" || "${alg_name}" == "cascada_sampling" ]]; then
-                    echo ">>>> Preparing data for ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
-                    PYTHONPATH=. python scripts/prepare_for_fine_tuning.py \
-                        --adversarial-dir ${dir} \
-                        --mix-with-path ${DATASETS_DIR}/${data_type}/${dataset}/target_clf/train.json \
-                        --num-examples ${num}
+        for dir in $(ls -d ${result_dir}/*); do
+            alg_name=$(basename ${dir})
+            echo ">>>> Preparing data for ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
+            PYTHONPATH=. python scripts/prepare_for_fine_tuning.py \
+                --adversarial-dir ${dir} \
+                --mix-with-path ${DATASETS_DIR}/${data_type}/${dataset}/target_clf/train.json \
+                --num-examples ${num}
 
 
-                    echo ">>>> Training ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
-                    export CLS_NUM_CLASSES="${datasets_num_labels[${dataset}]}"
-                    export CLS_TRAIN_DATA_PATH=${dir}/fine_tuning_data_${num}.json
-                    export CLS_VALID_DATA_PATH=${DATASETS_DIR}/${data_type}/${dataset}/target_clf/valid.json
+            echo ">>>> Training ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
+            export CLS_NUM_CLASSES="${datasets_num_labels[${dataset}]}"
+            export CLS_TRAIN_DATA_PATH=${dir}/fine_tuning_data_${num}.json
+            export CLS_VALID_DATA_PATH=${DATASETS_DIR}/${data_type}/${dataset}/target_clf/valid.json
 
-                    clf_dif=${LOGS_DIR}/${data_type}/dataset_${dataset}/target_clf/${alg_name}_${num}
-                    allennlp train configs/models/classifier/gru_classifier_no_vocab.jsonnet \
-                        -s ${clf_dif} \
-                        --force \
-                        --include-package adat
+            clf_dif=${LOGS_DIR}/${data_type}/dataset_${dataset}/target_clf/${alg_name}_${num}
+            allennlp train configs/models/classifier/gru_classifier_no_vocab.jsonnet \
+                -s ${clf_dif} \
+                --force \
+                --include-package adat
 
-                    echo ">>>> Evaluating classifier ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
-                    allennlp evaluate \
-                        ${clf_dif}/model.tar.gz \
-                        ${DATASETS_DIR}/${data_type}/${dataset}/target_clf/test.json \
-                        --include-package adat  \
-                        --output-file ${dir}/test_metrics_${num}.json \
-                        --cuda-device ${GPU_ID}
+            echo ">>>> Evaluating classifier ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
+            allennlp evaluate \
+                ${clf_dif}/model.tar.gz \
+                ${DATASETS_DIR}/${data_type}/${dataset}/target_clf/test.json \
+                --include-package adat  \
+                --output-file ${dir}/test_metrics_${num}.json \
+                --cuda-device ${GPU_ID}
 
-                    echo ">>>> Evaluating attack ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
-                    PYTHONPATH=. python scripts/evaluate_attack.py \
-                        --adversarial-dir ${dir} \
-                        --classifier-dir ${clf_dif} \
-                        --cuda ${GPU_ID}
-                fi
-            done
-        fi
+            echo ">>>> Evaluating attack ${dataset} dataset, ${alg_name} algorithm, ${num} examples"
+            PYTHONPATH=. python scripts/evaluate_attack.py \
+                --adversarial-dir ${dir} \
+                --classifier-dir ${clf_dif} \
+                --cuda ${GPU_ID}
+        done
     done
 done
 #done
